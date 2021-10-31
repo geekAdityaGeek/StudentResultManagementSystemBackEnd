f9622e9fd93ac80656e4a5006d5ef04659a284e9
Aditya Saha <adi96saha@gmail.com>
2021-10-31 22:13:37 +0530

    disable security and datasource creation

--- studentresult/src/main/java/com/management/student/studentresult/StudentresultApplication.java
+++ studentresult/src/main/java/com/management/student/studentresult/StudentresultApplication.java
@@ -1,9 +1,15 @@
 package com.management.student.studentresult;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+
+@Configuration
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class StudentresultApplication {
 
 	public static void main(String[] args) {
