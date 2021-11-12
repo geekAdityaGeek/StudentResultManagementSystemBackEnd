003efaac2cdb6b9515383bd0766ba8e0b8ebf9fd
Aditya Saha <adi96saha@gmail.com>
2021-11-12 20:15:36 +0530

    role apis shifted to RoleAction

--- studentresult/src/main/java/com/management/student/studentresult/controller/RoleActionController.java
+++ studentresult/src/main/java/com/management/student/studentresult/controller/RoleActionController.java
@@ -3,11 +3,16 @@ package com.management.student.studentresult.controller;
 import com.management.student.studentresult.dao.Role;
 import com.management.student.studentresult.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,13 +24,23 @@ public class RoleActionController {
     @PostMapping("/addRole")
     public Role addRole(@RequestBody Role role){
 
-//        Role newrole = new Role(role.getName(), role.getCreatedBy(), role.getModifiedBy());
-        return roleService.saveRole(role);
+        Role newrole = new Role(role.getName(), role.getCreatedBy(), role.getModifiedBy());
+        return roleService.saveRole(newrole);
     }
 
-//    @GetMapping("/allRoles")
-//    public List<Role> allRoles(){
-//        return roleService.getRoles();
-//    }
+    @GetMapping("/allRoles")
+    public @ResponseBody ResponseEntity<?> allRoles(){
+        List<String> roleNamesList= new ArrayList<>();
+        try {
+            List<Role> rolesList=roleService.getRoles();
+            for(int i=0;i<rolesList.size();i++)
+                roleNamesList.add(rolesList.get(i).getName());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<String>("Error in fetching Roles!", HttpStatus.EXPECTATION_FAILED);
+        }
+        return new ResponseEntity<List<String>>(roleNamesList,HttpStatus.OK);
+    }
 
 }
--- studentresult/src/main/java/com/management/student/studentresult/controller/UserController.java
+++ studentresult/src/main/java/com/management/student/studentresult/controller/UserController.java
@@ -36,36 +36,10 @@ public class UserController {
     @Autowired
     private RoleService roleService;
 
-  /**
-    @PostMapping("/addRole")
-    public Role addRole(@RequestBody Role role){
-
-        Role newrole = new Role(role.getName(), role.getCreatedBy(), role.getModifiedBy());
-        return roleService.saveRole(newrole);
-    }
-**/
     @PostMapping("/register")
     public @ResponseBody ResponseEntity<ResponseMessage> registerUser(@RequestBody UserDetails userDetails){
     	
     	return userService.registrationService(userDetails);
     }
 
-  /**
-  //place in RoleActionController.java
-    @GetMapping("/allRoles")
-    public @ResponseBody ResponseEntity<?> allRoles(){
-    	List<String> roleNamesList= new ArrayList<String>();
-    	try {
-			List<Role> rolesList=roleService.getRoles();
-			for(int i=0;i<rolesList.size();i++)
-				roleNamesList.add(rolesList.get(i).getName());
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
-			return new ResponseEntity<String>("Error in fetching Roles!",HttpStatus.EXPECTATION_FAILED);
-		}
-        return new ResponseEntity<List<String>>(roleNamesList,HttpStatus.OK);
-    }
-    **/
 }
