521711d0bbce73520cf4cbcf94cb65dd32bb0a34
bishwajeetsharma <bishwajeetsharma1999@gmail.com>
2021-12-11 18:20:42 +0530

    Updating validator.addNext function

--- studentresult/src/main/java/com/management/student/studentresult/validator/Validator.java
+++ studentresult/src/main/java/com/management/student/studentresult/validator/Validator.java
@@ -3,8 +3,9 @@ package com.management.student.studentresult.validator;
 public abstract class Validator implements ValidatorChain {
     private Validator nextValidator;
     public abstract void validateEntity() throws Exception;
-    public void addNext(Validator validator){
+    public Validator addNext(Validator validator){
         this.nextValidator = validator;
+        return this.nextValidator;
     }
     @Override
     public void validate() throws Exception {
--- studentresult/src/main/java/com/management/student/studentresult/validator/ValidatorChain.java
+++ studentresult/src/main/java/com/management/student/studentresult/validator/ValidatorChain.java
@@ -3,5 +3,5 @@ package com.management.student.studentresult.validator;
 public interface ValidatorChain {
 
     public void validate() throws Exception;
-    public void addNext(Validator validator);
+    public Validator addNext(Validator validator);
 }
